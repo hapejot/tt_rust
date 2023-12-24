ef05b014c151709693c2297f9c21852374e55ef9
Peter Jaeckel <jaeckel@acm.org>
2023-12-24 16:07:12 +0100

    fuser will only compile on unix/linx

--- src/agent/mod.rs
+++ src/agent/mod.rs
@@ -1,2 +1,3 @@
 pub mod protocol;
+#[cfg(unix)]
 pub mod fs;
--- src/bin/agentfs.rs
+++ src/bin/agentfs.rs
@@ -1,18 +1,25 @@
-use clap::Parser;
-use fuser::MountOption;
-use tracing::level_filters::LevelFilter;
-use tt_rust::{agent::fs::AgentFS, init_tracing};
-#[derive(Parser)]
-struct Args {
-    mount_point: String,
+#[cfg(unix)]
+mod main {
+    use clap::Parser;
+    use fuser::MountOption;
+    use tracing::level_filters::LevelFilter;
+    use tt_rust::{agent::fs::AgentFS, init_tracing};
+    #[derive(Parser)]
+    struct Args {
+        mount_point: String,
+    }
+    fn main() {
+        let args = Args::parse();
+        init_tracing("agentfs", LevelFilter::DEBUG);
+
+        let mut options = vec![MountOption::RW, MountOption::FSName("agent".to_string())];
+        options.push(MountOption::AutoUnmount);
+        options.push(MountOption::AllowRoot);
+        fuser::mount2(AgentFS::new(), args.mount_point, &options).unwrap();
+    }
 }
 
+#[cfg(windows)]
 fn main() {
-    let args = Args::parse();
-    init_tracing("agentfs", LevelFilter::DEBUG);
-
-    let mut options = vec![MountOption::RW, MountOption::FSName("agent".to_string())];
-    options.push(MountOption::AutoUnmount);
-    options.push(MountOption::AllowRoot);
-    fuser::mount2(AgentFS::new(), args.mount_point, &options).unwrap();
+    println!("user fs is not available on windows.")
 }
